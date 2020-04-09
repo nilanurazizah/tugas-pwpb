3ca0357bdd0b10be4c628d766a4b69c2c85b8a14
Nila Nur Azizah <55771272+nilanurazizah@users.noreply.github.com>
2020-04-09 10:16:32 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,9 @@
+  <?php
+
+echo"Nama   : Nila Nur Azizah"."\n";
+echo"Kelas  : XI RPL 1"."\n";
+echo"Sekolah: SMK Taruna Bhakti Depok"."\n";
+echo"No_hp  : 08997758215"."\n";
+echo"Email  : nurazizahnila10@gmail.com";
+
+?>
